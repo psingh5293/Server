858ed57f7dc6af000cf89b0b8fc91d3d79da6e06
Prakash Kumar <prakashkumar@users-MacBook-Pro.local>
2019-02-04 18:26:27 +0530

    router.js changes

--- router.js
+++ router.js
@@ -6,9 +6,8 @@ const requireAuth = passport.authenticate('jwt',{session:false});
 const requireSignIn = passport.authenticate('local',{session:false});
 module.exports = function(app){
     app.get('/',requireAuth,function(req,res,next){
-        res.send(['prakash','singh','1993'])
+        res.send(['prakash','singh','1993','last '])
     })
-
    app.post('/signin',requireSignIn,Authentication.signIn)
     app.post('/signup',Authentication.signUp)
 }
