b8206123e685c59c975ac412e6e516622dbab000
Prakash Kumar <psingh5293@gmail.com>
2019-02-16 17:45:21 +0530

    Revert "1. Updating router.js"
    This reverts commit f2e9603661989d75d1130c1c4ecf583897d19c6d.

--- router.js
+++ router.js
@@ -1,13 +1,13 @@
-const Authentication = require("./controllers/authentication");
-const passportService = require("./services/passport");
-const passport = require("passport");
+const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
 
-const requireAuth = passport.authenticate("jwt", { session: false });
-const requireSignIn = passport.authenticate("local", { session: false });
-module.exports = function(app) {
-  app.get("/reduxauth", requireAuth, function(req, res, next) {
-    res.send(["prakash", "singh", "1993", "last "]);
-  });
-  app.post("/signin", requireSignIn, Authentication.signIn);
-  app.post("/signup", Authentication.signUp);
-};
+const requireAuth = passport.authenticate('jwt',{session:false});
+const requireSignIn = passport.authenticate('local',{session:false});
+module.exports = function(app){
+    app.get('/',requireAuth,function(req,res,next){
+        res.send(['prakash','singh','1993','last '])
+    })
+   app.post('/signin',requireSignIn,Authentication.signIn)
+    app.post('/signup',Authentication.signUp)
+}
