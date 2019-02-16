f2e9603661989d75d1130c1c4ecf583897d19c6d
Prakash Kumar <psingh5293@gmail.com>
2019-02-16 17:37:04 +0530

    1. Updating router.js

--- models/user.js
+++ models/user.js
@@ -1,36 +1,42 @@
-const mongoose= require('mongoose');
-const Schema = mongoose.Schema
-const bcrypt = require('bcrypt-nodejs')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 //define our  model
 
 const userSchema = new Schema({
-email:{type:String,unique:true,lowercase:true},
-password : String
-})
+  email: { type: String, unique: true, lowercase: true },
+  password: String
+});
 
-userSchema.pre('save',function(next){
-    const user = this;
-    bcrypt.genSalt(10,function(err,salt){
-        if(err) { return next(err)}
-   
-    bcrypt.hash(user.password,salt,null,function(err,hash){
-        if(err) { return next(err)}
-        user.password = hash;
-        next();
-    })
-})
+userSchema.pre("save", function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
 
-})
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
 
-userSchema.methods.comparePassword = function(candidatePassword,callback){
-    bcrypt.compare(candidatePassword,this.password , function(err,isMatch){
-        if(err) { return callback(err)}
-        else { return callback(null,isMatch)}
-    })
-}
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    } else {
+      return callback(null, isMatch);
+    }
+  });
+};
 //create the modal class
 
-const modalClass = mongoose.model('user',userSchema); 
+const modalClass = mongoose.model("user", userSchema);
 
 //export the modal
-module.exports = modalClass
+module.exports = modalClass;
