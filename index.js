6b2fb81007aad44e41ba668d47e71da327797c63
Prakash Kumar <psingh5293@gmail.com>
2019-02-16 17:31:50 +0530

    1. Updating index

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
-const router =require('./router')
-const mongoose = require('mongoose');
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 //DB SET UP
 
-mongoose.connect('mongodb://localhost/auth');
+mongoose.connect("mongodb://localhost/auth");
 
 //APP SETUP
 //app.use(morgan('combined'))
-app.use(bodyParser.json({type:'*/*'}))
+app.use(cors());
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 //server set up
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
-server.listen(port)
-console.log('server is running at',port)
-
+server.listen(port);
+console.log("server is running at", port);
